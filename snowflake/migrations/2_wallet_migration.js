dc95a91124591e745c2c6265d69c17811f86e29c
Njoku Emmanuel <kalunjoku123@gmail.com>
2020-08-17 12:26:57 +0100

    Clean up migrations

--- snowflake/migrations/2_wallet_migration.js
+++ snowflake/migrations/2_wallet_migration.js
@@ -13,6 +13,7 @@ module.exports = async (deployer) => {
   const snowflake = await SnowflakeContract.at(
     "0x47aC2F343926868e892Ba53a9D09e98bf6124460"
   );
+
   //@params uniswapVia contract, snowflake address,
   await deployer.deploy(
     UniswapVia,
@@ -24,27 +25,21 @@ module.exports = async (deployer) => {
   console.log("Uniswap via: ", UniswapVia.address);
   const factory = await FactoryContract.at(FactoryContract.address);
   console.log("Factory contract address: ", factory.address);
-
-  await snowflake
-    .addResolver(FactoryContract.address, true, allowance, "0x00")
-    .then(console.log("\n Deploying protected wallet..."));
-  let protectedWalletAddr = await factory.getWalletByEIN(4);
-  console.log("\n Protected wallet at: ", protectedWalletAddr, "\n");
-  const wallet = await ProtectedWallet.at(protectedWalletAddr);
-  await snowflake.addResolver(wallet.address, true, 0, "0x00");
-  const deposit = web3.utils.toBN(10000).mul(web3.utils.toBN(1e18));
-  await snowflake.changeResolverAllowances([wallet.address], [deposit]);
-
-  // Deposits hydro from snowflake
-  await wallet.depositFromSnowflake(deposit);
-  const withdrawal1 = web3.utils.toBN(90).mul(web3.utils.toBN(1e18));
-  console.log(" Attempting withdrawal beneath daily limit...");
-  await wallet.withdrawToSnowflake(withdrawal1);
-  console.log(" Success \n");
-  await wallet.resetChainlinkState();
-  console.log(
-    " Your wallet at address: ",
-    wallet.address,
-    " Write this address into chainlinktest.js"
-  );
+  /*
+    await snowflake.addResolver(FactoryContract.address, true, allowance, "0x00").then(console.log("\n Deploying protected wallet..."))
+    let protectedWalletAddr = await factory.getWalletByEIN(4)
+    console.log("\n Protected wallet at: ", protectedWalletAddr, "\n")
+    const wallet = await ProtectedWallet.at(protectedWalletAddr)
+    await snowflake.addResolver(wallet.address, true, 0, "0x00")
+    const deposit = web3.utils.toBN(10000).mul(web3.utils.toBN(1e18));
+    await snowflake.changeResolverAllowances([wallet.address], [deposit])
+    // Deposits hydro from snowflake
+    await wallet.depositFromSnowflake(deposit)
+    const withdrawal1 = web3.utils.toBN(90).mul(web3.utils.toBN(1e18))
+    console.log(" Attempting withdrawal beneath daily limit...")
+    await wallet.withdrawToSnowflake(withdrawal1)
+    console.log(" Success \n")
+    await wallet.resetChainlinkState()
+    console.log(" Your wallet at address: ", wallet.address, " Write this address into chainlinktest.js")
+    */
 };
